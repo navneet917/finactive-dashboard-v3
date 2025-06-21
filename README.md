64cb7dda80eb4152c40bb70c66d81e8d3fe60470
navneet917 <917.navneet@gmail.com>
2025-06-21 13:01:17 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+This folder contains the Streamlit implementation for the module: 8 AI Assistant
---
+++ app.py
@@ -0,0 +1,3 @@
+# Finactive Streamlit Main App
+import streamlit as st
+st.title('Finactive - Personal Finance Dashboard')
